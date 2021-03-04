5d3c7ead03d86c90ec26b6e2c863201828fb58b7
in-roma <romain.grandjean@outlook.com>
2021-03-04 15:26:17 +0100

    homepage structure

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,28 +1,38 @@
 import React from 'react';
+import './homepage.scss';
 
 const HomePage = () => (
 	<div className="home-page">
-		<div className="menu"></div>
-		<div className="content">
-			<div className="content-small-frame">
-				<h1 className="title">Hats</h1>
-				<span className="subtitle">SHOW NOW</span>
+		<div className="page-contents">
+			<div className="menu-item">
+				<div className="content">
+					<h1 className="title">Hats</h1>
+					<span className="subtitle">SHOW NOW</span>
+				</div>
 			</div>
-			<div className="content-small-frame">
-				<h1 className="title">Jackets</h1>
-				<span className="subtitle">SHOW NOW</span>
+			<div className="menu-item">
+				<div className="content">
+					<h1 className="title">Jackets</h1>
+					<span className="subtitle">SHOW NOW</span>
+				</div>
 			</div>
-			<div className="content-small-frame">
-				<h1 className="title">Sneakers</h1>
-				<span className="subtitle">SHOW NOW</span>
+			<div className="menu-item">
+				<div className="content">
+					<h1 className="title">Sneakers</h1>
+					<span className="subtitle">SHOW NOW</span>
+				</div>
 			</div>
-			<div className="content-large-frame">
-				<h1 className="title">Womens</h1>
-				<span className="subtitle">SHOW NOW</span>
+			<div className="menu-item">
+				<div className="content">
+					<h1 className="title">Womens</h1>
+					<span className="subtitle">SHOW NOW</span>
+				</div>
 			</div>
-			<div className="content-large-frame">
-				<h1 className="title">Mens</h1>
-				<span className="subtitle">SHOW NOW</span>
+			<div className="menu-item">
+				<div className="content">
+					<h1 className="title">Mens</h1>
+					<span className="subtitle">SHOW NOW</span>
+				</div>
 			</div>
 		</div>
 	</div>
