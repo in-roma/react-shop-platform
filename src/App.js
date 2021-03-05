6b49547d6144098e851538b7aef5d2291c3de6a9
Romain Grandjean <romain.grandjean@outlook.com>
2021-03-05 20:12:18 +0100

    implementing header

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
 // Components
+import Header from './components/header/header.jsx';
 import HomePage from './pages/homePage/homePage.jsx';
 import ShopPage from './pages/shopPage/shopPage.jsx';
 
 function App() {
 	return (
 		<React.Fragment>
+			<Header />
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route exact path="/shop" component={ShopPage} />
---
+++ src/components/header/header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './header.scss';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+
+const Header = () => (
+	<div className="header">
+		<Link to="/" className="logo-container">
+			<Logo className="logo" />
+		</Link>
+		<div className="header-sections">
+			<Link to="/shop" className="header-section">
+				Shop
+			</Link>
+			<Link to="/contact" className="header-section">
+				Contact
+			</Link>
+		</div>
+	</div>
+);
+
+export default Header;
