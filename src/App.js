78cbbaa22966b43fa57d4a3b6d899f0a6fa1f25a
Romain Grandjean <romain.grandjean@outlook.com>
2021-03-20 21:40:44 +0100

    setting up user reducer, implementing user within app and header components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 // Components
@@ -8,31 +9,27 @@ import Header from './components/header/header.jsx';
 import HomePage from './pages/homePage/homePage.jsx';
 import ShopPage from './pages/shopPage/shopPage.jsx';
 import SignInPage from './pages/signInPage/signInPage.jsx';
+// Redux
+import { setCurrentUser } from './redux/user/user.action';
 
 class App extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			currentUser: null,
-		};
-	}
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
+		const { setCurrentUser } = this.props;
+
 		this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
 			if (userAuth) {
 				const userRef = await createUserProfileDocument(userAuth);
 
 				userRef.onSnapshot((snapShot) => {
-					this.setState({
-						currentUser: {
-							id: snapShot.id,
-							...snapShot.data(),
-						},
+					setCurrentUser({
+						id: snapShot.id,
+						...snapShot.data(),
 					});
 				});
 			}
-			this.setState({ currentUser: userAuth });
+			setCurrentUser(userAuth);
 		});
 	}
 
@@ -43,7 +40,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<React.Fragment>
-				<Header currentUser={this.state.currentUser} />
+				<Header />
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route exact path="/shop" component={ShopPage} />
@@ -54,4 +51,8 @@ class App extends React.Component {
 	}
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+	setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(null, mapDispatchToProps)(App);
