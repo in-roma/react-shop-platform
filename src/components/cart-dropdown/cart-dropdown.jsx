a2f5a447191b8ac4e33826b231c8c81734c6af12
Romain Grandjean <romain.grandjean@outlook.com>
2021-03-22 17:37:22 +0100

    cartItem component plus implementation within dropdown

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import './cart-dropdown.scss';
 
 // Components
 import Button from '../button/button';
+import CartItem from '../cart-item/cartItem';
 
-const CartDropDown = () => (
+const CartDropDown = ({ cartItems }) => (
 	<div className="cart-dropdown">
-		<div className="cart-items"></div>
+		<div className="cart-items">
+			{cartItems.map((cartItem) => (
+				<CartItem key={cartItem.id} item={cartItem} />
+			))}
+		</div>
 		<Button>GO TO CHECKOUT</Button>
 	</div>
 );
 
-export default CartDropDown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+	cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropDown);
