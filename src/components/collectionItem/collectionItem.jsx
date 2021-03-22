5cca38378152277f4e1e64c4da57a3a70a967b89
Romain Grandjean <romain.grandjean@outlook.com>
2021-03-22 17:18:23 +0100

    updated shop data, updated card utils

--- src/components/collectionItem/collectionItem.jsx
+++ src/components/collectionItem/collectionItem.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import './collectionItem.scss';
+import { connect } from 'react-redux';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+// Components
+import Button from '../button/button';
+// Redux components
+import { addItem } from '../../redux/cart/cart.action';
+
+const CollectionItem = ({ item, addItem }) => (
 	<div className="collection-item">
 		<div
 			className="image"
-			style={{ backgroundImage: `url(${imageUrl})` }}
+			style={{ backgroundImage: `url(${item.imageUrl})` }}
 		></div>
 		<div className="collection-footer">
-			<span className="name">{name}</span>
-			<span className="price">{price}.00$</span>
+			<span className="name">{item.name}</span>
+			<span className="price">{item.price}.00$</span>
 		</div>
+		<Button onClick={() => addItem(item)} inverted>
+			Add to cart
+		</Button>
 	</div>
 );
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+	addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/collectionPreview/collectionPreview.jsx
+++ src/components/collectionPreview/collectionPreview.jsx
@@ -11,8 +11,8 @@ const CollectionPreview = ({ title, items }) => (
 		<div className="preview">
 			{items
 				.filter((item, i) => i < 4)
-				.map(({ id, ...otherSectionProps }) => (
-					<CollectionItem key={id} {...otherSectionProps} />
+				.map((item) => (
+					<CollectionItem key={item.id} item={item} />
 				))}
 		</div>
 	</div>
