5595690b8a3eea9f0ebd3c4dc4afc88fc8ba3919
Romain Grandjean <romain.grandjean@outlook.com>
2021-03-23 16:09:51 +0100

    implementing memoized selectors with reselect library on components cartdropDown and cartIcon

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 import './cart-dropdown.scss';
 
 // Components
@@ -18,8 +18,8 @@ const CartDropDown = ({ cartItems }) => (
 	</div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-	cartItems,
+const mapStateToProps = (state) => ({
+	cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps)(CartDropDown);
