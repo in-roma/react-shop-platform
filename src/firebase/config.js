cfc332b87f7783d9fd6c4a120bbf2d055c34cc8d
Romain Grandjean <romain.grandjean@outlook.com>
2021-03-28 14:27:44 +0200

    fixing heroku bug deployment

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+const config = {
+	apiKey: 'AIzaSyAnJs8mGn2n50RtFWeULPjv1O4yrDF4DAA',
+	authDomain: 'react-shop-platform.firebaseapp.com',
+	projectId: 'react-shop-platform',
+	storageBucket: 'react-shop-platform.appspot.com',
+	messagingSenderId: '1084155808521',
+	appId: '1:1084155808521:web:ad40bb7d7c8961a94fa1d8',
+};
+
+export default config;
