157959bdc4ddf5f4cfd3274823450e462a84d7fe
Romain Grandjean <romain.grandjean@outlook.com>
2021-03-29 18:04:23 +0200

    setting up firebase db fetching

--- src/pages/shopPage/shopPage.jsx
+++ src/pages/shopPage/shopPage.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+// Firestore
+import {
+	firestore,
+	convertCollectionsSnapShotToMap,
+} from '../../firebase/firebase.utils';
+
 // Components
 import CollectionOverview from '../../components/collectionOverview/collectionOverview';
 import CollectionPage from '../collectionPage/collectionPage';
@@ -8,14 +14,35 @@ import CollectionPage from '../collectionPage/collectionPage';
 // Stylesheet
 import './shopPage.scss';
 
-const ShopPage = ({ match }) => (
-	<div className="shop-page">
-		<Route exact path={`${match.path}`} component={CollectionOverview} />
-		<Route
-			path={`${match.path}/:collectionId`}
-			component={CollectionPage}
-		/>
-	</div>
-);
+class ShopPage extends React.Component {
+	unsubscribeFromSnapShot = null;
+
+	componentDidMount() {
+		const collectionRef = firestore.collection('collections');
+		this.unsubscribeFromSnapShot = collectionRef.onSnapshot(
+			async (snapShot) => {
+				const collectionMap = convertCollectionsSnapShotToMap(snapShot);
+				console.log(collectionMap);
+			}
+		);
+	}
+
+	render() {
+		const { match } = this.props;
+		return (
+			<div className="shop-page">
+				<Route
+					exact
+					path={`${match.path}`}
+					component={CollectionOverview}
+				/>
+				<Route
+					path={`${match.path}/:collectionId`}
+					component={CollectionPage}
+				/>
+			</div>
+		);
+	}
+}
 
 export default ShopPage;
