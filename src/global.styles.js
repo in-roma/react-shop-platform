298ff4cb85dc9a1f876c1c891551c2f944222a74
Romain Grandjean <romain.grandjean@outlook.com>
2021-04-02 19:27:43 +0200

    mobile layout implementation

--- src/global.styles.js
+++ src/global.styles.js
@@ -8,5 +8,14 @@ export const GlobalStyle = createGlobalStyle`
 body {
     font-family: 'Inter', sans-serif;
     padding: 24px;
+    padding: 20px 40px;
+
+    @media screen and (max-width: 800px) {
+        padding: 10px;
+    }
+}
+a {
+    text-decoration: none;
+    color: black;
 }
   `;
