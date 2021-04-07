f0d5e0e32ad7d293582c2db6c0fbb49b3c12f889
Romain Grandjean <romain.grandjean@outlook.com>
2021-04-07 18:29:04 +0200

    fixing shopPage layout & responsive

--- src/components/collectionPreview/collectionPreview.jsx
+++ src/components/collectionPreview/collectionPreview.jsx
@@ -8,7 +8,9 @@ import './collectionPreview.scss';
 
 const CollectionPreview = ({ title, items }) => (
 	<div className="collection-preview">
-		<h1 className="title">{title.toUpperCase()}</h1>
+		<div className="preview-title">
+			<div className="title">{title.toUpperCase()}</div>
+		</div>
 		<div className="preview">
 			{items
 				.filter((item, i) => i < 4)
