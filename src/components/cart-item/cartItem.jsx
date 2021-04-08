eff007c2bb26c15b9a346aa1abf38e3123c551de
Romain Grandjean <romain.grandjean@outlook.com>
2021-04-08 15:39:29 +0200

    fixing cart responsive layout

--- src/components/cart-item/cartItem.jsx
+++ src/components/cart-item/cartItem.jsx
@@ -5,7 +5,9 @@ import './cartItem.scss';
 
 const cartItem = ({ item: { imageUrl, price, name, quantity } }) => (
 	<div className="cart-item">
-		<img src={imageUrl} alt="item" />
+		<div className="cart-image">
+			<img src={imageUrl} alt="item" />
+		</div>
 		<div className="item-details">
 			<span className="name">{name}</span>
 			<span className="price">
